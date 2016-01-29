3a4fb3d2c60d0a413cfee42ec67919db97109eb5
Whde <460290973@qq.com>
2016-01-29 16:29:03 +0800

    add
    First Add

---
+++ Language/Language/Language.h
@@ -0,0 +1,19 @@
+//
+//  Language.h
+//  Language
+//
+//  Created by whde on 16/1/13.
+//  Copyright © 2016年 Whde. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Language.
+FOUNDATION_EXPORT double LanguageVersionNumber;
+
+//! Project version string for Language.
+FOUNDATION_EXPORT const unsigned char LanguageVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Language/PublicHeader.h>
+
+
---
+++ Language/Products/Language.framework/Headers/CLanguageUtil.h
@@ -0,0 +1,30 @@
+//
+//  CLanguageUtil.h
+//  eCarry
+//
+//  Created by whde on 16/1/6.
+//  Copyright © 2016年 whde. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+typedef NS_OPTIONS (NSInteger, Language){
+    Language_Default,
+    Language_EN,
+    Language_Hans,
+    Language_Hant
+};
+#define LANGUAGE_CHANGE_NOTIFICATION @"LANGUAGE_CHANGE_NOTIFICATION"
+#define CLocalizedString(key) NSLocalizedStringFromTableInBundle(key, @"Localizable", [CLanguageUtil bundle], nil)
+#define CLocalizedNibName(key) [CLanguageUtil localizedNibName:key]
+#define CLocalizedImgName(key) NSLocalizedStringFromTableInBundle(key, @"ImageLocalized", [CLanguageUtil bundle], nil)
+@interface CLanguageUtil : NSObject
++(NSBundle *)bundle;//获取当前资源文件
+
+// 获取当前语言
++ (Language)getCurrentLanguage;
+
+// 设置语言
++ (void)setCurrentLanguage:(Language)language;
+
++ (NSString *)localizedNibName:(NSString *)key;
+@end
